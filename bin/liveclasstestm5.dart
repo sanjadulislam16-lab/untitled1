cc734d6dc512c454372a05e54a6421b8c4599eba
sanjadulislam16-lab <sanjadulislam16@gmail.com>
2025-09-16 00:01:48 +0600

    Update liveclasstestm5.dart

--- bin/liveclasstestm5.dart
+++ bin/liveclasstestm5.dart
@@ -1,1 +1,56 @@
-dart.main
+class Student{
+  String name;
+  int roll;
+
+  Student(this.name, this.roll);
+
+  display(){
+    print('Name:$name\nRoll: $roll');
+  }
+}
+void main(){
+  Student obj = Student('Suaib', 23147);
+  obj.display();
+}
+
+
+class Person {
+  String name;
+
+  Person(this.name);
+}
+
+class Teacher extends Person {
+  String Subject;
+  Teacher(super.name,this.Subject);
+
+  teacherObject(){
+    print('Name:$name\nSubject:$Subject');
+  }
+}
+void mains(){
+  Teacher obj2 = Teacher("Sun", "Math");
+  obj2.teacherObject();
+
+}
+
+
+import 'dart:math';
+
+abstract class Shape{
+  double area();
+}
+
+class Circle extends Shape{
+  double redius;
+  Circle(this.redius);
+
+  double area(){
+    return pi *redius*redius;
+  }
+}
+
+void mainn(){
+  Circle obj3 = Circle(7);
+  print('Area Of the Circle: ${obj3.area()}');
+}
